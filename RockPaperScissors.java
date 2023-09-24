bd054f50c3c015f2a8b2288340f1587d06f630bc
TedoGeorgiev <145587055+TedoGeorgiev@users.noreply.github.com>
2023-09-24 10:58:40 +0300

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -9,48 +9,60 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose [r]ock, [p]aper or [s]cissors ");
+        int wins = 0;
+        int loses = 0;
+        int draws = 0;
+
+        System.out.println("Choose [r]ock, [p]aper, [s]cissors or [E]nds the game");
         String playerMove = scanner.nextLine();
 
-        if (playerMove.equals("r") || playerMove.equals("rock")) {
-            playerMove = ROCK;
-        } else if (playerMove.equals("p") || playerMove.equals("paper")) {
-            playerMove = PAPER;
-        } else if (playerMove.equals("s") || playerMove.equals("scissors")) {
-            playerMove = SCISSORS;
-        } else {
-            System.out.println("Invalid input. Try again...");
-            return;
+        while (!"E".equalsIgnoreCase(playerMove)) {
+
+            if (playerMove.equals("r") || playerMove.equals("rock")) {
+                playerMove = ROCK;
+            } else if (playerMove.equals("p") || playerMove.equals("paper")) {
+                playerMove = PAPER;
+            } else if (playerMove.equals("s") || playerMove.equals("scissors")) {
+                playerMove = SCISSORS;
+            } else {
+                System.out.println("Invalid input. Try again...");
+                return;
+            }
+
+            Random random = new Random();
+            int computerRandomNumber = random.nextInt(3);
+            String computerMove = "";
+
+            switch (computerRandomNumber) {
+                case 0:
+                    computerMove = "Rock";
+                    break;
+                case 1:
+                    computerMove = "Paper";
+                    break;
+                case 2:
+                    computerMove = "Scissors";
+                    break;
+            }
+            System.out.printf("The computer choose %s.%n", computerMove);
+
+            if ((playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) ||
+                    (playerMove.equals(PAPER) && computerMove.equals(ROCK)) ||
+                    (playerMove.equals(SCISSORS) && computerMove.equals(PAPER))) {
+                ++wins;
+                System.out.println("You win.");
+            } else if ((computerMove.equals(ROCK) && playerMove.equals(SCISSORS)) ||
+                    (computerMove.equals(PAPER) && playerMove.equals(ROCK)) ||
+                    (computerMove.equals(SCISSORS) && playerMove.equals(PAPER))) {
+                ++loses;
+                System.out.println("You lose.");
+            } else {
+                ++draws;
+                System.out.println("This game was a draw.");
+            }
+            System.out.println("Choose [r]ock, [p]aper, [s]cissors or [E]nds the game");
+            playerMove = scanner.nextLine();
         }
-
-        Random random = new Random();
-        int computerRandomNumber = random.nextInt(3);
-        String computerMove = "";
-
-        switch (computerRandomNumber) {
-            case 0:
-                computerMove = "Rock";
-                break;
-            case 1:
-                computerMove = "Paper";
-                break;
-            case 2:
-                computerMove = "Scissors";
-                break;
-        }
-        System.out.printf("The computer choose %s.%n", computerMove);
-
-        if ((playerMove.equals(ROCK) && computerMove.equals(SCISSORS)) ||
-        (playerMove.equals(PAPER) && computerMove.equals(ROCK)) ||
-                (playerMove.equals(SCISSORS) && computerMove.equals(PAPER))) {
-            System.out.println("You win.");
-        } else if ((computerMove.equals(ROCK) && playerMove.equals(SCISSORS)) ||
-                (computerMove.equals(PAPER) && playerMove.equals(ROCK)) ||
-                (computerMove.equals(SCISSORS) && playerMove.equals(PAPER))) {
-            System.out.println("You lose.");
-        } else {
-            System.out.println("This game was a draw.");
-        }
-
+        System.out.printf("Game Ends with result: %d Wins, %d Loses, %d Draws", wins, loses, draws);
     }
 }
